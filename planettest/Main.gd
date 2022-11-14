90f756f5fdab2a0d22bc1b793de9f32253b37c15
Chesapeake <root@moth.monster>
2022-11-14 02:38:11 -0500

    initial commit

---
+++ planettest/Main.gd
@@ -0,0 +1,8 @@
+extends Spatial
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE

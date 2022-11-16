a9d968e9be9c90e481c7c8ead18f923cb4c349b5
Chesapeake <root@moth.monster>
2022-11-16 01:34:08 -0500

    Delete planettest directory

--- planettest/Main.gd
+++
@@ -1,8 +0,0 @@
-extends Spatial
-
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
